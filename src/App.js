59c54872d5d8b0ff242fb3ed7b163b5cc90d74a5
Joshua Ho <ho.joshua4@gmail.com>
2019-09-12 13:14:37 -0700

    pypi blueprint completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
 
+import PypiCard from './components/Pypi'
 // import PypiCard from '/components/Pypi'
-
 function App() {
   return (
     <Router>
- 
       <div>
         <ul>
           <li>
@@ -24,6 +24,14 @@ function App() {
   );
 }
 
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
 class Feed extends React.Component{
 
   constructor(props){
@@ -35,19 +43,21 @@ class Feed extends React.Component{
   }
 
   componentDidMount(){
-    fetch("https://pyn-terest.herokuapp.com/")
-      .then(res => res.json())
-      .then( (result) => {
-        this.setState( {
-          loadedData: true,
-          data: result
-        });
-        console.log('RESUUUUUUUULT',this.state)
-        console.log('RESUUUUUUUULT ONE:',this.state.data[0])
-      })
-      .catch( res => {
-        console.log('Error Message:', res)
-      })
+
+    if (!this.state.loadedData){
+      fetch("https://pyn-terest.herokuapp.com/")
+        .then(res => res.json())
+        .then( (result) => {
+          this.setState( {
+            loadedData: true,
+            data: result
+          });
+        })
+        .catch( res => {
+          console.log('Error Message:', res)
+        }
+      )
+    }
   }
 
   render(){
@@ -56,131 +66,23 @@ class Feed extends React.Component{
     if (this.state.loadedData){
       
       return (
-        <div>
+        <div id="card-display">
           {
             this.state.data[0].data.map( function(post, index){
-              console.log('POST!!!!!!!',post)
               return <PypiCard data={post}/>    
             })
           }
         </div>
       )
-      
     }
-
-    // { 
-    //   this.state[0].map( (post, index) => {
-    //     return <PypiCard postData={post} />    
-    //   })
-    // }
 
     return (
       <div>
         <h2>Home</h2>
-        
-        {/* <PypiCard /> */}
         {githubCards}
       </div>
     );
   }
 }
-
-// function Feed() {
-//   return (
-//     <div>
-//       <h2>Home</h2>
-//       <PypiCard />
-//     </div>
-//   );
-// }
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Topic({ match }) {
-  return (
-    <div>
-      <h3>{match.params.topicId}</h3>
-    </div>
-  );
-}
-
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
-// class PypiCard extends React.Component {
-  // constructor(props){
-  //   super(props)
-  //   this.state = {
-  //     source: "Pypi",
-  //     category: props.category, 
-  //     title: props.title,
-  //     description: props.description,
-  //     url: props.url
-  //   }
-  // }
-  // render() {
-  //   return (
-  //     <div>
-  //       <a href={this.state.url}>
-  //         <h1>{this.state.title}</h1>
-  //         <p>{this.state.category}</p>
-  //         <p>{this.state.description}</p>
-  //       </a>
-  //     </div>
-  //    )
-  // }
-  // }
-
-
-  
-function PypiCard(props){
-
-  let styles = {
-    pypiCard: {
-      "border": "1px solid black"
-    }
-  }
-
-  return (
-    <div style={styles.pypiCard} >
-      <a href={props.data.link} target="_blank">
-        <h1>{props.data.title}</h1>
-        <p>Popular</p>
-        <p>{props.data.desc}
-        </p>
-      </a>
-    </div>
-   )
-}
-
 
 export default App;
--- src/components/Pypi/index.js
+++ src/components/Pypi/index.js
@@ -1,32 +1,34 @@
-import React, { Component } from './node_modules/react';
+import React, { Component } from 'react';
+import "./pypi_card.css"
 
-class PypiCard extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      source: "Pypi",
-      category: props.category, 
-      title: props.title,
-      description: props.description,
-      url: props.url
+
+const pythonIcon = require("../../img/pythonIcon.png")
+
+function PypiCard(props){
+  let styles = {
+    pypiCard: {
+      // "border": "1px solid black",
+      // "borderRadius": "15px",
+      // "margin": "10px 0",
+      // "padding": "5px 10px",
+      // "width": "30%",
     }
   }
 
-  render() {
-    return (
-      <div>
-        <a href={this.state.url}>
-          <h1>{this.state.title}</h1>
-          <p>{this.state.category}</p>
-          <p>{this.state.description}</p>
-        </a>
-      </div>
-     )
-  }
-
+  return (
+    <div style={styles.pypiCard} className="card card-pypi" >
+      <a href={props.data.link} target="_blank">
+        <div className='card-content'>
+          <h1>{props.data.title}</h1>
+          <p>{props.data.desc}</p>
+        </div>
+        <div className="card-footer">
+          <p>Popular</p>
+          <img src={pythonIcon}></img>
+        </div>
+      </a>
+    </div>
+   )
 }
-
-
-
 
 export default PypiCard
